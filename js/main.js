c71f2bfed9ffb51f6994837409b16e125e361016
Artem Ugryumov <artiom.ygryumov@gmail.com>
2021-08-09 12:28:34 +0300

    -

--- js/main.js
+++ js/main.js
@@ -101,7 +101,6 @@ function searchHeight() {
 
   const headerHeight = header.scrollHeight
   search.style.height = headerHeight + 'px'
-  console.log(headerHeight)
 }
 
 closeWhenClickingOnBg('.search__body', '', document.querySelector('.search'));
@@ -138,6 +137,191 @@ function searchClose() {
   })
 }
 
+// Слайдер на главном экране
+const mainSlider = new Swiper('.main__slider', {
+  
+  slidesPerView: 1, // Кол-во показываемых слайдов
+  spaceBetween: 0, // Расстояние между слайдами
+  // loop: true, // Бесконечный слайдер
+  // freeMode: true, // Слайдеры не зафиксированны
+  // touchMoveStopPropagation: false,
+  // noSwiping: false,
+  allowTouchMove: false,
+
+  breakpoints: {
+    1200: {
+
+    },
+    700: {
+
+    },
+    400: {
+
+    }
+  },
+
+  pagination: {
+    el: 'main__slider__pagination',
+    clickable: true,
+  },
+});
+
+// Слайдер в разделе отзывы
+const reviewsSlider = new Swiper('.reviews__body', { 
+  
+  slidesPerView: 3, // Кол-во показываемых слайдов
+  spaceBetween: 14, // Расстояние между слайдами
+  // allowTouchMove: false,
+
+  breakpoints: {
+    1200: {
+
+    },
+    700: {
+
+    },
+    400: {
+
+    }
+  },
+
+  pagination: {
+    el: '.reviews__cards__pagination',
+    clickable: true,
+  },
+});
+
+// Инпуты
+// Если инпут не будет пустой, элементу с классом input будет присвоен класс _valid
+validInput();
+function validInput() {
+  const elems = document.querySelectorAll('.input');
+
+  for (let i = 0; i < elems.length; i++) {
+    const input = elems[i].querySelector('input');
+
+    input.addEventListener('input', () => {
+      if (input.value != '') {
+        input.parentElement.classList.add('_valid');
+      }
+      else {
+        input.parentElement.classList.remove('_valid');
+      }
+    })
+  }
+}
+
+// Если инпут с классом _req будет пустым, ему присвоется класс _error
+requiredInput();
+function requiredInput() {
+  const inputElems = document.querySelectorAll('._req');
+
+  for (let i = 0; i < inputElems.length; i++) {
+    const input = inputElems[i];
+
+    input.addEventListener('input', () => {
+      if (input.value == '') {
+        input.parentElement.classList.add('_error');
+      }
+      else {
+        input.parentElement.classList.remove('_error');
+      }
+    })
+  }
+}
+
+// Первая буква каждого слова будет в высоком регистре
+upperCaseAllFirstLetter('input');
+function upperCaseAllFirstLetter(inputs) {
+  const inputElems = document.querySelectorAll(inputs);
+
+  for (let i = 0; i < inputElems.length; i++) {
+    const input = inputElems[i];
+   
+    input.addEventListener('input', () => {
+      input.value = input.value.replace(/(\s|^)[а-яёa-z]/g, function(match) {
+        return match.toUpperCase();
+      });
+    })
+  }
+}
+
+// В инпуте могут быть только буквы английские и русские
+validInputName();
+function validInputName() {
+  const inputElems = document.querySelectorAll('input[name="user_name"]');
+
+  for (let i = 0; i < inputElems.length; i++) {
+    const input = inputElems[i];
+    
+    input.addEventListener('keypress', function(e) {
+      const inputCharCode = e.charCode;
+      if((inputCharCode >= 48 && inputCharCode <= 57) && (inputCharCode != 43 && inputCharCode != 0 && inputCharCode != 40 && inputCharCode != 41 && inputCharCode != 45)) {
+        e.preventDefault();
+      }
+    });
+  }
+  // inputElems.forEach(input => {
+  // })
+}
+
+// В начале не может быть пробела. 2 пробела подряд меняются на один
+inputSpace();
+function inputSpace() {
+  const inputElems = document.querySelectorAll('input, textarea');
+  for (let i = 0; i < inputElems.length; i++) {
+    const input = inputElems[i];
+    
+    input.addEventListener('keypress', (e) => {
+      if (input.value.length < 1) {
+        const inputCharCode = e.charCode;
+        if (inputCharCode == 32) {
+          e.preventDefault();
+        }
+      }
+    })
+    
+    input.addEventListener('input', () => {
+      input.value = input.value.replace(/\s\s+/g, ' ');
+    })
+  }
+}
+
+calcSelectClass();
+function calcSelectClass() {
+  const cardElems = document.querySelectorAll('.calc__class-card')
+
+  for (let i = 0; i < cardElems.length; i++) {
+    const card = cardElems[i];
+    card.addEventListener('click', () => {
+      for (let i = 0; i < cardElems.length; i++) {
+        const cardAll = cardElems[i];
+        cardAll.classList.remove('_selected');
+      }
+      card.classList.add('_selected');
+
+    })
+  }
+}
+
+// Отображение tooltip
+tooltip()
+function tooltip() {
+  const elems = document.querySelectorAll('[data-tooltip]')
+
+  for (let i = 0; i < elems.length; i++) {
+    const elem = elems[i];
+    const data = elem.dataset.tooltip
+    const elemClass = elem.getAttribute('class')
+    const tooltip = document.createElement('span')
+
+    tooltip.classList.add('tooltip')
+    tooltip.classList.add(elemClass + '-tooltip')
+    tooltip.innerText = data
+
+    elem.append(tooltip)
+  }
+}
 
 // // Модальные окна
 // if (document.querySelector('.modal') && document.querySelector('[data-open-modal]')) {
